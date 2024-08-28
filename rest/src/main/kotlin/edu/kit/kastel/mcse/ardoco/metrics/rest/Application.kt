55bfcdb84d213993e53049128b3a6a9bd5015e14
Dominik Fuchß <dominik.fuchss@kit.edu>
2024-08-28 17:21:07 +0200

    Enhance OpenAPI Definition

--- rest/src/main/kotlin/edu/kit/kastel/mcse/ardoco/metrics/rest/Application.kt
+++ rest/src/main/kotlin/edu/kit/kastel/mcse/ardoco/metrics/rest/Application.kt
@@ -1,11 +1,21 @@
 package edu.kit.kastel.mcse.ardoco.metrics.rest
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@OpenAPIDefinition(info = Info(title = "ArDoCo: Metrics", description = "API for calculating metrics for TLR and other tasks"))
+@OpenAPIDefinition(
+    info =
+        Info(
+            title = "ArDoCo: Metrics",
+            description = "This tool provides functionality to calculate and aggregate classification and rank metrics for various tasks.",
+            license = License(name = "MIT License", url = "https://github.com/ArDoCo/Metrics/blob/main/LICENSE.md")
+        ),
+    externalDocs = ExternalDocumentation(description = "ArDoCo: Metrics - Wiki", url = "https://github.com/ArDoCo/metrics/wiki")
+)
 @SpringBootApplication
 class Application
 
